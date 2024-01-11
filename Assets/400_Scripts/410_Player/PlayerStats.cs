95c3e494971dc7299f7804e4077f3b05b6a6fb85
Alexis DARRAS <alexis.darras@brassart.fr>
2024-01-11 14:20:45 +0100

    Debug canvaStat

--- Assets/400_Scripts/410_Player/PlayerStats.cs
+++ Assets/400_Scripts/410_Player/PlayerStats.cs
@@ -5,4 +5,14 @@ using UnityEngine;
 public class PlayerStats : MonoBehaviour
 {
     public int moneyCount;
+    public static PlayerStats Instance;
+
+    public void Awake()
+    {
+        if (Instance == null)
+        {
+            Instance = this;
+        }
+    }
+
 }
