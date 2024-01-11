30f562114890fddf67bfb3c9b0ffb3ca0892d2ab
Valerian Lambert <valerian.lambert13@gmail.com>
2024-01-11 11:07:50 +0100

    Création du player et controller
    RT Analogique pour la puissance
    Stick gauche pour la direction
    "A" pour tirer la balle

---
+++ Assets/400_Scripts/210_Player/PlayerController.cs
@@ -0,0 +1,89 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerController : MonoBehaviour
+{
+    public float ThrowStrenght;
+    [Tooltip("Modifiez cette variable pour augmenter ou baisser la force de propulsion")]
+    public int StrenghMultiplier = 40;
+    public Vector2 PivotValue;
+    [Space]
+    public InputActionAsset ActionAsset;
+    public GameObject Pivot;
+
+    private InputAction ArrowDirection;
+    private InputAction ArrowStrenght;
+
+    private Vector3 strenghtToScale;
+    private Quaternion pivotToRotation;
+    private float angle;
+
+    private Rigidbody rb;
+    private Vector3 lastVel;
+
+    private void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+
+        InputActionMap ActionMap = ActionAsset.FindActionMap("Gamepad");
+
+        ArrowDirection = ActionMap.FindAction("Arrow Direction");
+
+        ArrowStrenght = ActionMap.FindAction("Strenght Modifier");
+
+        ActionMap.FindAction("Throw Player").performed += ThrowPlayer;
+    }
+
+    private void ThrowPlayer(InputAction.CallbackContext ctx)
+    {
+        Vector3 forceDirection = Quaternion.Euler(0f, angle, 0f) * Vector3.forward;
+        Debug.Log(forceDirection);
+        rb.AddForce(forceDirection * ThrowStrenght, ForceMode.Impulse);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        pivotToRotation = Pivot.transform.rotation;
+        strenghtToScale = Pivot.transform.localScale;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ThrowStrenght = ArrowStrenght.ReadValue<float>() * StrenghMultiplier;
+        PivotValue = ArrowDirection.ReadValue<Vector2>();
+
+        angle = Mathf.Atan2(PivotValue.x, PivotValue.y) * Mathf.Rad2Deg;
+        Pivot.transform.rotation = pivotToRotation;
+        Pivot.transform.rotation = Quaternion.Euler(0, angle, 0);
+
+        strenghtToScale.z = ThrowStrenght / (StrenghMultiplier / 5);
+        Pivot.transform.localScale = strenghtToScale;
+
+        lastVel = rb.velocity;
+    }
+
+    private void OnEnable()
+    {
+        ActionAsset.FindActionMap("Gamepad").Enable();
+    }
+
+    private void OnDisable()
+    {
+        ActionAsset.FindActionMap("Gamepad").Disable();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 7)
+        {
+            float speed = lastVel.magnitude;
+            Vector3 direction = Vector3.Reflect(lastVel.normalized, collision.contacts[0].normal);
+            rb.velocity = direction * Mathf.Max(speed, 0f);
+        }
+    }
+}
