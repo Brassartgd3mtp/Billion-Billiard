1100303ebb837008397b81becf3260b98022f267
Alexis DARRAS <alexis.darras@brassart.fr>
2024-02-06 16:44:26 +0100

    Setup pour LevelManager

---
+++ Assets/400_Scripts/480_GameManager/LevelManager.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class LevelManager
+{
+    public static bool Unlocked_Lv_1;
+    public static bool Unlocked_Lv_2;
+    public static bool Unlocked_Lv_3;
+    public static bool Unlocked_Lv_4;
+    public static bool Unlocked_Lv_5;
+    public static bool Unlocked_Lv_6;
+    public static bool Unlocked_Lv_7;
+    public static bool Unlocked_Lv_8;
+    public static bool Unlocked_Lv_9;
+}
