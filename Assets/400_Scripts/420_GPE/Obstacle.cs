7fa7ed77dc8dad482b55e613ee6757938bbb9bb5
Valerian Lambert <valerian.lambert13@gmail.com>
2024-02-07 14:34:50 +0100

    Convoyer Belt V2 + Update Obstacles
    # Conflicts:
    #	Assets/400_Scripts/420_GPE/Obstacle.cs

--- Assets/400_Scripts/420_GPE/Obstacle.cs
+++ Assets/400_Scripts/420_GPE/Obstacle.cs
@@ -6,9 +6,26 @@ public class Obstacle : MonoBehaviour
 {
     public enum ObstacleType
     {
-        Concrete,
-        Rubber,
-        NPC
+        /// <summary>
+        /// Concrete wall type, default bounce value : .8
+        /// </summary>
+        [InspectorName("Béton")] Concrete,
+        /// <summary>
+        /// Rubber wall type, default bounce value : .98
+        /// </summary>
+        [InspectorName("Caoutchouc")] Rubber,
+        /// <summary>
+        /// Rubber wall type, default bounce value : .5
+        /// </summary>
+        [InspectorName("Feutre")] Felt,
+        /// <summary>
+        /// Rubber wall type, Player will follow the shape.
+        /// </summary>
+        [InspectorName("Glace")] Ice,
+        /// <summary>
+        /// Rubber wall type, default Unity physics bouce.
+        /// </summary>
+        [InspectorName("PNJ")] NPC
     }
 
     public ObstacleType obstacleType = ObstacleType.Rubber;        
