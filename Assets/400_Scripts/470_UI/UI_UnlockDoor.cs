b5b2cfff2c4108918e198e05eccca4d4795da00b
Nicolas IMBERT <nicolas.imbert@brassart.fr>
2024-02-09 16:33:04 +0100

    Debut Script feedback de la porte verrouillée

--- Assets/400_Scripts/470_UI/UI_UnlockDoor.cs
+++ Assets/400_Scripts/470_UI/UI_UnlockDoor.cs
@@ -4,19 +4,25 @@ using UnityEngine;
 
 public class UI_UnlockDoor : MonoBehaviour
 {
-    private Vector3 targetPosition;
-    private RectTransform pointerRectTransform;
+    public Transform Origin;
+    public Transform Target;
 
-    private void Awake()
+    public Transform IconPadlockUnlock;
+    void Update()
     {
-        targetPosition = new Vector3(200, 45);
-        pointerRectTransform = transform.Find("Pointer").GetComponent<RectTransform>();
-    }
-    private void Update()
-    {
-        Vector3 toPosition = targetPosition;
-        Vector3 formPosition = Camera.main.transform.position;
-        formPosition.z = 0f;
-        Vector3 dir = (toPosition - formPosition).normalized;
+        if (Origin == null || Target == null)
+        {
+            Debug.LogError("Veuillez assigner l'origine et la cible dans l'inspecteur.");
+            return;
+        }
+
+        // Calcul de la direction de la cible par rapport à l'origine
+        Vector3 direction = (Target.position - Origin.position);
+
+        IconPadlockUnlock.transform.position = new Vector3(direction.x/ Target.position.x*1920, direction.z/ Target.position.z*-1080, 0);
+
+        Debug.Log("Direction de la cible par rapport à l'origine : " + direction);
+        //float angle = Vector3.Angle(Origin.forward, direction);
+        //Debug.Log("Angle entre l'axe avant de l'origine et la direction de la cible : " + angle);
     }
 }
