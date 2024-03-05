7329dcc914cd33e0ed0dad8cebfde0ea5ff65b5a
Valerian Lambert <valerian.lambert13@gmail.com>
2024-03-05 14:58:32 +0100

    PlayerFreeCam modif d'ergonomie
    + Ajout du déplacement en étant en dézoom

--- Assets/400_Scripts/410_Player/PlayerFreeCam.cs
+++ Assets/400_Scripts/410_Player/PlayerFreeCam.cs
@@ -9,98 +9,78 @@ public class PlayerFreeCam : MonoBehaviour
     [Tooltip("Distance à laquelle la caméra peut aller.\nValeur par défaut : 10")]
     public float CameraSpeed = 50;
 
-    //[SerializeField] CinemachineBrain camBrain;
+    [SerializeField] CinemachineBrain camBrain;
     [SerializeField] CinemachineVirtualCamera camFollow;
-    [SerializeField] CinemachineVirtualCamera camTranslate;
     [SerializeField] CinemachineVirtualCamera camFullView;
 
-    CinemachineTransposer camT;
+    CinemachineTransposer camFVT;
+    CinemachineBlendDefinition camBlendDef;
 
-    //InputAction.CallbackContext freeCam;
-    //bool mouseFreeCamEnabled = false;
+    InputAction.CallbackContext freeCam;
+    bool isFreeCamActive = false;
 
     private void Awake()
     {
-        camT = camTranslate.GetCinemachineComponent<CinemachineTransposer>();
+        camFVT = camFullView.GetCinemachineComponent<CinemachineTransposer>();
+        camBlendDef = camBrain.m_DefaultBlend;
     }
 
-    //public void FreeCam(InputAction.CallbackContext context)
-    //{
-    //    freeCam = context;
-    //
-    //    if (mouseFreeCamEnabled || Gamepad.current != null)
-    //    {
-    //        camTranslate.gameObject.SetActive(true);
-    //        camFollow.gameObject.SetActive(false);
-    //    }
-    //    
-    //    if (context.canceled)
-    //    {
-    //        StartCoroutine(CameraReturn());
-    //    }
-    //}
+    public void FreeCam(InputAction.CallbackContext context)
+    {
+        freeCam = context;
+    }
 
-    //private bool isFreeCamEnabled = false;
     public void StartFreeCam(InputAction.CallbackContext context)
     {
-        Debug.Log("it works !");
-        //isFreeCamEnabled = !isFreeCamEnabled;
-
         if (context.started)
         {
             camFollow.gameObject.SetActive(false);
             camFullView.gameObject.SetActive(true);
+            camBlendDef.m_Time = 0f;
+
+            isFreeCamActive = true;
         }
         
         if (context.canceled)
         {
-            camFullView.gameObject.SetActive(false);
-            camFollow.gameObject.SetActive(true);
+            camBlendDef.m_Time = .2f;
+            StartCoroutine(CameraReturn());
+
+            isFreeCamActive = false;
         }
-        //if (Gamepad.current == null)
-        //{
-        //    mouseFreeCamEnabled = true;
-        //    
-        //    if (context.canceled)
-        //    {
-        //        mouseFreeCamEnabled = false;
-        //    
-        //        StartCoroutine(CameraReturn());
-        //    }
-        //}
     }
 
-    //private IEnumerator CameraReturn()
-    //{
-    //    while (camT.m_FollowOffset != new Vector3(0, 20, 0))
-    //    {
-    //        camT.m_FollowOffset.x = Mathf.MoveTowards(camT.m_FollowOffset.x, 0, Time.deltaTime * 300);
-    //        camT.m_FollowOffset.z = Mathf.MoveTowards(camT.m_FollowOffset.z, 0, Time.deltaTime * 300);
-    //
-    //        yield return new WaitForSeconds(.01f);
-    //    }
-    //
-    //    camTranslate.gameObject.SetActive(false);
-    //    camFollow.gameObject.SetActive(true);
-    //
-    //    yield break;
-    //}
-    //
-    //private void Update()
-    //{
-    //    if (Gamepad.current != null)
-    //    {
-    //        camT.m_FollowOffset.x += freeCam.ReadValue<Vector2>().x * CameraSpeed * Time.deltaTime * 5;
-    //        camT.m_FollowOffset.z += freeCam.ReadValue<Vector2>().y * CameraSpeed * Time.deltaTime * 5;
-    //    }
-    //    
-    //    else if (mouseFreeCamEnabled)
-    //    {
-    //        camT.m_FollowOffset.x += freeCam.ReadValue<Vector2>().x * CameraSpeed * Time.deltaTime;
-    //        camT.m_FollowOffset.z += freeCam.ReadValue<Vector2>().y * CameraSpeed * Time.deltaTime;
-    //    }
-    //
-    //    camT.m_FollowOffset.x = Mathf.Clamp(camT.m_FollowOffset.x, -20, 20);
-    //    camT.m_FollowOffset.z = Mathf.Clamp(camT.m_FollowOffset.z, -11, 11f);
-    //}
+    private IEnumerator CameraReturn()
+    {
+        while (camFVT.m_FollowOffset != new Vector3(0, 50, 0))
+        {
+            camFVT.m_FollowOffset.x = Mathf.MoveTowards(camFVT.m_FollowOffset.x, 0, Time.deltaTime * 300);
+            camFVT.m_FollowOffset.z = Mathf.MoveTowards(camFVT.m_FollowOffset.z, 0, Time.deltaTime * 300);
+    
+            yield return new WaitForSeconds(.01f);
+        }
+
+        camFullView.gameObject.SetActive(false);
+        camFollow.gameObject.SetActive(true);
+    
+        yield break;
+    }
+    
+    private void Update()
+    {
+        if (Gamepad.current != null && isFreeCamActive)
+        {
+            camFVT.m_FollowOffset.x += freeCam.ReadValue<Vector2>().x * CameraSpeed * Time.deltaTime * 5;
+            camFVT.m_FollowOffset.z += freeCam.ReadValue<Vector2>().y * CameraSpeed * Time.deltaTime * 5;
+        }
+        
+        else if (isFreeCamActive)
+        {
+            camFVT.m_FollowOffset.x += freeCam.ReadValue<Vector2>().x * CameraSpeed * Time.deltaTime;
+            camFVT.m_FollowOffset.z += freeCam.ReadValue<Vector2>().y * CameraSpeed * Time.deltaTime;
+        }
+    
+        camFVT.m_FollowOffset.x = Mathf.Clamp(camFVT.m_FollowOffset.x, -300, 300);
+        camFVT.m_FollowOffset.z = Mathf.Clamp(camFVT.m_FollowOffset.z, -300, 300f);
+    }
 }
