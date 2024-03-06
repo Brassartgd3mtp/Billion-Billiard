cc06ecaf89a0aefbae26eea0dc15a8b214d62ccd
Nicolas IMBERT <nicolas.imbert@brassart.fr>
2024-03-06 17:17:16 +0100

    Merge Conflict

---
+++ Assets/400_Scripts/410_Player/PlayerParameters.cs
@@ -0,0 +1,66 @@
+using Assets.SimpleLocalization.Scripts;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerParameters : MonoBehaviour
+{
+    private PlayerController playerController;
+
+    private Rigidbody rb;
+
+    [Tooltip("Masse du player, la masse du Rigidbody se set au start à partir de cette valeur")]
+    public float playerMass;
+
+    [Tooltip("Courbe de la valeur de Drag sur le temps. Elle débute lorsque la valeur de Speed est inférieure à speedForStartingDrag")]
+    public AnimationCurve dragCurve;
+    public float timerOfCurve;
+
+    [Tooltip("Valeur de la Speed à partir de laquelle la courbe de Drag débute")]
+    public float speedForStartingDrag;
+    private bool startDrag;
+    private float timerForDrag;
+
+    private Vector3 vel;
+    [SerializeField] private float speed;
+    private bool canDrag;
+
+
+    public void Awake()
+    {
+        rb = GetComponent<Rigidbody>();
+        playerController = GetComponent<PlayerController>();
+    }
+
+    public void Start()
+    {
+        rb.mass = playerMass;
+    }
+
+    public void Update()
+    {
+        vel = rb.velocity;
+        speed = vel.magnitude;
+
+        if (playerController.isShooted)
+        {
+            timerOfCurve = 0;
+            timerForDrag -= Time.deltaTime;
+            canDrag = false;
+
+            if (timerForDrag <= 0)
+            {
+                playerController.isShooted = false;
+                timerForDrag = 0.5f;
+                canDrag= true;
+            }
+        }
+
+        if (speed < speedForStartingDrag && canDrag)
+        {
+            timerOfCurve += Time.deltaTime;
+            rb.drag = dragCurve.Evaluate(timerOfCurve);
+        }
+        else rb.drag = 1;
+    }
+}
---
+++ Assets/Obsolete/InputManager.cs
@@ -0,0 +1,123 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class InputManagerd : MonoBehaviour
+{
+    PlayerActionMap actions;
+
+    private NoClip noClip;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        actions = new PlayerActionMap();
+
+        if (gameObject.TryGetComponent(out PlayerController playerController))
+        {
+            #region Gamepad
+            TurnBasedSystem.OnEnablePlayerInput += actions.Gamepad.GamepadStrenght.Enable;
+            TurnBasedSystem.OnEnablePlayerInput += actions.Gamepad.ThrowPlayer.Enable;
+            TurnBasedSystem.OnDisablePlayerInput += actions.Gamepad.GamepadStrenght.Disable;
+            TurnBasedSystem.OnDisablePlayerInput += actions.Gamepad.ThrowPlayer.Disable;
+
+            //actions.Gamepad.ThrowPlayer.performed += playerController.GamepadThrow;
+            actions.Gamepad.GamepadStrenght.performed += playerController.GamepadStrenght;
+            actions.Gamepad.GamepadStrenght.canceled += playerController.GamepadStrenght;
+            #endregion
+            #region Mouse/Keyboard
+            TurnBasedSystem.OnEnablePlayerInput += actions.MouseKeyboard.MouseStartDrag.Enable;
+            TurnBasedSystem.OnDisablePlayerInput += actions.MouseKeyboard.MouseStartDrag.Disable;
+
+            actions.MouseKeyboard.MouseStrenght.performed += playerController.MouseStrenght;
+            actions.MouseKeyboard.MouseStartDrag.started += playerController.MouseStartDrag;
+            actions.MouseKeyboard.MouseStartDrag.performed += playerController.MouseStartDrag;
+            actions.MouseKeyboard.MouseStartDrag.canceled += playerController.MouseStartDrag;
+            actions.MouseKeyboard.MouseCancelThrow.performed += playerController.MouseCancelThrow;
+            #endregion
+        }
+
+        if (gameObject.TryGetComponent(out PlayerFreeCam playerFreeCam))
+        {
+            //actions.Gamepad.FreeCam.performed += playerFreeCam.FreeCam;
+            //actions.Gamepad.FreeCam.canceled += playerFreeCam.FreeCam;
+            //actions.MouseKeyboard.FreeCam.performed += playerFreeCam.FreeCam;
+            actions.MouseKeyboard.StartFreeCam.performed += playerFreeCam.StartFreeCam;
+            actions.MouseKeyboard.StartFreeCam.canceled += playerFreeCam.StartFreeCam;
+        }
+
+        if (gameObject.TryGetComponent(out ReloadScene reloadScene))
+        {
+            actions.Cheat.ReloadScene.performed += reloadScene.Reload;
+        }
+
+        if (gameObject.TryGetComponent(out NoClip noClip))
+        {
+            this.noClip = noClip;
+            actions.Cheat.NoClip.performed += NoClipMode;
+            actions.Cheat.NoClipControl.performed += noClip.MovePlayer;
+            actions.Cheat.NoClipControl.canceled += noClip.MovePlayer;
+        }
+
+        actions.Gamepad.Enable();
+        if (Gamepad.current == null)
+            actions.MouseKeyboard.Enable();
+        actions.Cheat.Enable();
+    }
+
+    private void NoClipMode(InputAction.CallbackContext context)
+    {
+        if (!noClip.ModeOn)
+        {
+            noClip.ModeOn = true;
+            actions.Gamepad.Disable();
+            actions.MouseKeyboard.Disable();
+            noClip.PlayerCollider.enabled = false;
+        }
+        else
+        {
+            noClip.ModeOn = false;
+            actions.Gamepad.Enable();
+            actions.MouseKeyboard.Enable();
+            noClip.PlayerCollider.enabled = true;
+        }
+    }
+
+    private void OnDisable()
+    {
+        if (gameObject.TryGetComponent(out PlayerController playerController))
+        {
+            #region Gamepad
+            //actions.Gamepad.ThrowPlayer.performed -= playerController.GamepadThrow;
+            actions.Gamepad.GamepadStrenght.performed -= playerController.GamepadStrenght;
+            actions.Gamepad.GamepadStrenght.canceled -= playerController.GamepadStrenght;
+            #endregion
+            #region Mouse/Keyboard
+            actions.MouseKeyboard.MouseStrenght.performed -= playerController.MouseStrenght;
+            actions.MouseKeyboard.MouseStartDrag.started -= playerController.MouseStartDrag;
+            actions.MouseKeyboard.MouseStartDrag.performed -= playerController.MouseStartDrag;
+            actions.MouseKeyboard.MouseStartDrag.canceled -= playerController.MouseStartDrag;
+            actions.MouseKeyboard.MouseCancelThrow.performed -= playerController.MouseCancelThrow;
+            #endregion
+        }
+
+        if (gameObject.TryGetComponent(out PlayerFreeCam playerFreeCam))
+        {
+            //actions.Gamepad.FreeCam.performed -= playerFreeCam.FreeCam;
+            //actions.Gamepad.FreeCam.canceled -= playerFreeCam.FreeCam;
+            //actions.MouseKeyboard.FreeCam.performed -= playerFreeCam.FreeCam;
+            actions.MouseKeyboard.StartFreeCam.performed -= playerFreeCam.StartFreeCam;
+            actions.MouseKeyboard.StartFreeCam.canceled -= playerFreeCam.StartFreeCam;
+        }
+
+        if (gameObject.TryGetComponent(out ReloadScene reloadScene))
+        {
+            actions.Cheat.ReloadScene.performed -= reloadScene.Reload;
+        }
+
+        if (gameObject.TryGetComponent(out NoClip noClip))
+        {
+            actions.Cheat.NoClip.performed -= NoClipMode;
+            actions.Cheat.NoClipControl.performed -= noClip.MovePlayer;
+            actions.Cheat.NoClipControl.canceled -= noClip.MovePlayer;
+        }
+    }
+}
