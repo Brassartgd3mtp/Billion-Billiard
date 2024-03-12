eff77071b84584794750a56a9c10b1dd4493977a
Hugo PETRUZZELLI <hugo.petruzzelli@brassart.fr>
2024-03-12 09:50:56 +0100

    Modif Audio Mixer
    Création du script pour gerer les sons

---
+++ Assets/400_Scripts/440_AudioManager/HugoAudio/AudioManager2.cs
@@ -0,0 +1,74 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+using System; 
+
+public class AudioManager2 : MonoBehaviour
+{
+
+
+    public static AudioManager2 Instance;
+
+
+    public Sound[] musicSounds, sfxSounds; 
+    public AudioSource musicSource, sfxSource;
+
+
+    public void Awake()
+    {
+       if (Instance == null)
+        {
+            Instance = this; 
+            DontDestroyOnLoad(gameObject);
+        }
+
+       else
+        {
+            Destroy(gameObject);
+        }
+    }
+
+    private void Start()
+    {
+        PlayMusic("Theme");
+    }
+
+  
+
+
+    public void PlayMusic(string name)
+    {
+        Sound s = Array.Find(musicSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("Sound not found");
+        }
+
+        else
+            {
+                musicSource.clip = s.clip; 
+                musicSource.Pause();
+            }
+        
+    }
+
+
+    public void PlaySDFX(string name)
+    {
+        Sound s = Array.Find(sfxSounds, x => x.name == name);
+
+        if (s == null)
+        {
+            Debug.Log("SFX not found");
+        }
+
+        else
+        {
+            sfxSource.PlayOneShot(s.clip); 
+        }
+    }
+
+
+}
