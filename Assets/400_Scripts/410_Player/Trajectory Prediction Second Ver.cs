cd2e2bf925d1d5c22e02c98eb8aab033e9407f90
Valerian <valerian.lambert13@gmail.com>
2024-03-14 21:08:47 +0100

    Traj Pred v0.7 Reflect (non fonctionnel)

--- Assets/400_Scripts/410_Player/Trajectory Prediction Second Ver.cs
+++ Assets/400_Scripts/410_Player/Trajectory Prediction Second Ver.cs
@@ -5,7 +5,7 @@ using UnityEngine.InputSystem;
 public class TrajectoryPredictionSecVer : MonoBehaviour
 {
     [SerializeField] private LineRenderer lineRenderer;
-    //[SerializeField] private LineRenderer reflectedLine;
+    [SerializeField] private LineRenderer reflectedLine;
 
     PlayerController playerController;
 
@@ -13,6 +13,7 @@ public class TrajectoryPredictionSecVer : MonoBehaviour
     {
         playerController = GetComponent<PlayerController>();
     }
+
     RaycastHit hit;
     private void Update()
     {
@@ -21,9 +22,18 @@ public class TrajectoryPredictionSecVer : MonoBehaviour
         {
             Vector3 contactPos = new Vector3(0, 0, Vector3.Distance(hit.point, transform.position));
             lineRenderer.SetPosition(1, contactPos);
+
+            reflectedLine.startWidth = .5f;
+            reflectedLine.SetPosition(0, contactPos);
+            reflectedLine.SetPosition(1, Vector3.Reflect(ray.direction, hit.normal) + hit.point);
         }
         else
+        {
             lineRenderer.SetPosition(1, Vector3.forward * playerController.ThrowStrength / 4);
+
+            reflectedLine.SetPosition(0, Vector3.zero);
+            reflectedLine.SetPosition(1, Vector3.zero);
+        }
 
         lineRenderer.startWidth = 1 / (40 / playerController.ThrowStrength);
     }
@@ -45,10 +55,4 @@ public class TrajectoryPredictionSecVer : MonoBehaviour
     //{
     //    return transform.forward * playerController.ThrowStrength / 2;
     //}
-
-    Vector3 ReflectPosition(Vector3 position, Vector3 normal, Vector3 hitPoint)
-    {
-        Vector3 reflection = Vector3.Reflect(position - hitPoint, normal);
-        return hitPoint + reflection;
-    }
 }
