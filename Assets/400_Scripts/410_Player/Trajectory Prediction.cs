6217f77c07735ef8251f439ebc53545afa9c5f3f
Valerian Lambert <valerian.lambert13@gmail.com>
2024-03-15 15:19:29 +0100

    Trajectory Prediction v1

--- Assets/400_Scripts/410_Player/Trajectory Prediction.cs
+++ Assets/400_Scripts/410_Player/Trajectory Prediction.cs
@@ -1,79 +1,35 @@
-using System;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
 public class TrajectoryPrediction : MonoBehaviour
 {
-    public int predictionSteps = 30;
-    public float predictionStepInterval = 0.1f;
-    public GameObject predictionPointPrefab;
+    [SerializeField] private LineRenderer lineRenderer;
 
-    private GameObject[] predictionPoints;
     PlayerController playerController;
-
-    Vector3 currentPos;
 
     void Start()
     {
         playerController = GetComponent<PlayerController>();
-
-        predictionPoints = new GameObject[predictionSteps];
-
-        Vector3 pointsPos = new Vector3(transform.position.x, 0f, transform.position.z);
-
-        for (int i = 0; i < predictionSteps; i++)
-        {
-            predictionPoints[i] = Instantiate(predictionPointPrefab, pointsPos, Quaternion.identity, transform);
-        }
     }
 
+    RaycastHit hit;
     private void Update()
     {
-        currentPos = transform.position;
-    }
-
-    public void Predict(InputAction.CallbackContext context)
-    {
-        if (context.performed)
+        Ray ray = new Ray(transform.position, transform.forward);
+        if (Physics.Raycast(ray, out hit, playerController.ThrowStrength / 4))
         {
-            for (int i = 0; i < predictionSteps; i++)
-            {
-                float time = i * predictionStepInterval;
-                Vector3 newPosition = currentPos + CalculateVelocity() * time;
-                newPosition.y = .5f;
+            lineRenderer.SetPosition(1, transform.InverseTransformPoint(hit.point));
 
-                if (i > 0)
-                {
-                    float maxDist = Vector3.Distance(predictionPoints[i - 1].transform.position, predictionPoints[i].transform.position);
+            lineRenderer.positionCount = 3;
 
-                    RaycastHit hit;
-                    if (Physics.Raycast(predictionPoints[i - 1].transform.position, newPosition - predictionPoints[i - 1].transform.position, out hit, maxDist))
-                    {
-                        newPosition = ReflectPosition(newPosition, hit.normal, hit.point);
-                    }
-                }
-
-                predictionPoints[i].transform.position = newPosition;
-            }
+            lineRenderer.SetPosition(2, transform.InverseTransformPoint(hit.point + Vector3.Reflect(ray.direction, hit.normal) * 3));
         }
-    }
-
-    public void CancelPredict(InputAction.CallbackContext context)
-    {
-        foreach (GameObject point in predictionPoints)
+        else
         {
-            point.transform.localPosition = new Vector3(0, -.5f, 0);
+            lineRenderer.positionCount = 2;
+
+            lineRenderer.SetPosition(1, Vector3.forward * playerController.ThrowStrength / 4);
         }
-    }
 
-    Vector3 CalculateVelocity()
-    {
-        return transform.forward * playerController.ThrowStrength / 2;
-    }
-
-    Vector3 ReflectPosition(Vector3 position, Vector3 normal, Vector3 hitPoint)
-    {
-        Vector3 reflection = Vector3.Reflect(position - hitPoint, normal);
-        return hitPoint + reflection;
+        //lineRenderer.startWidth = 1 / (40 / playerController.ThrowStrength);
     }
 }
