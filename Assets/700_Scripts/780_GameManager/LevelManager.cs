dc620c2af1f34fdecc9c524b996081285c965afa
Valerian Lambert <valerian.lambert13@gmail.com>
2024-03-25 11:43:12 +0100

    Level Manager dans le prefab Scene Base

---
+++ Assets/700_Scripts/780_GameManager/LevelManager.cs
@@ -0,0 +1,68 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelManager : MonoBehaviour
+{
+    public LevelType levelType = LevelType.HoleInOne;
+    private Scene currentScene;
+
+    private void Start()
+    {
+        if (levelType == LevelType.HoleInOne)
+        {
+            currentScene = SceneManager.GetActiveScene();
+
+            TurnBasedPlayer.Instance.shotRemaining = 1;
+            TurnBasedPlayer.Instance.nbrOfShots = 1;
+
+            TurnBasedPlayer.Instance.PassiveReloadEnabled = false;
+            TurnBasedPlayer.Instance.PassiveReloadCooldown = 1;
+
+            StartCoroutine(HoleInOne());
+        }
+    }
+
+    //private void Update()
+    //{
+    //    if (levelType == LevelType.HoleInOne)
+    //    {
+    //        if (PlayerController.rb.velocity.magnitude <= 0
+    //        && TurnBasedPlayer.Instance.shotRemaining == 0)
+    //        {
+    //            SceneManager.LoadScene(currentScene.buildIndex);
+    //        }
+    //    }
+    //}
+
+    IEnumerator HoleInOne()
+    {
+        if (PlayerController.rb.velocity.magnitude > 0)
+        {
+            while (true)
+            {
+                if (PlayerController.rb.velocity.magnitude <= 0.001f
+                && TurnBasedPlayer.Instance.shotRemaining == 0)
+                {
+                    SceneManager.LoadScene(currentScene.buildIndex);
+                    yield break;
+                }
+                else
+                    yield return null;
+            }
+        }
+        else
+        {
+            yield return new WaitForSeconds(.1f);
+            StartCoroutine(HoleInOne());
+            yield break;
+        }
+    }
+}
+
+public enum LevelType
+{
+    HoleInOne,
+    Exploration
+}
