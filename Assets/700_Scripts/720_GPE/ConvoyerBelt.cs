55a68c3cf96e53e51a7524b97d7dee8842e39c15
Nicolas <imbert.nicolas0@gmail.com>
2024-03-29 14:47:57 +0100

    Convoyer Belt révision du script - Animation Cameraman Idle
    Révision du script du Convoyer Belt
    - Pour changer la direction il faut modifier les valeurs en X et Y de la variable Direction
    - La variable Apply Force augmente la force appliquée sur le joueur ou NPC
    - Speed Texture augmente la vitesse de défilement de la texture, n'influe pas la force ou le joueur
    - Tiling une façon de disposer des formes planes identiques de manière à ce qu'elles couvrent entièrement une surface sans qu'il y ait de différence entre les deux

--- Assets/700_Scripts/720_GPE/ConvoyerBelt.cs
+++ Assets/700_Scripts/720_GPE/ConvoyerBelt.cs
@@ -1,26 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting.Dependencies.Sqlite;
 using UnityEngine;
 
 public class ConvoyerBelt : MonoBehaviour
 {
-    [SerializeField, Space] private GameObject startPoint;
-    [SerializeField] private GameObject endPoint;
-    private Vector3 direction;
+    private MeshRenderer meshRenderer;
+    [SerializeField, Range(0, 5), Space] private float speedTexture;
+    [SerializeField, Range(1, 50)] float applyForce = 10;
+    [SerializeField] private Vector2 Tiling;
+    [SerializeField] private Vector2 Direction;
+    private MaterialPropertyBlock materialPropertyBlock;
+    private float rotateTexture;
+    private Vector3 forceDirection;
 
-    [SerializeField, Range(0, 100), Space] private float speed;
-
-    void Update()
+    private void Start()
     {
-        float directionX = endPoint.transform.position.x - startPoint.transform.position.x;
-        float directionZ = endPoint.transform.position.z - startPoint.transform.position.z;
-
-        direction = new Vector3(directionX, 0, directionZ);
+        InitPropertyBlock();
     }
 
     private void OnTriggerStay(Collider other)
     {
         if (other.gameObject.TryGetComponent(out Rigidbody _colrb))
-            _colrb.AddForce(direction * speed * 2);
+            _colrb.AddForce(transform.TransformDirection(forceDirection) * applyForce);
     }
+
+    private void InitPropertyBlock()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+
+        rotateTexture = Vector2.SignedAngle(Vector2.right, Direction);
+        forceDirection = new Vector3(Direction.x, 0, Direction.y).normalized;
+        
+        materialPropertyBlock = new MaterialPropertyBlock();
+        meshRenderer.GetPropertyBlock(materialPropertyBlock);
+        
+        materialPropertyBlock.SetFloat("_Rotate", rotateTexture);
+        materialPropertyBlock.SetVector("_Tiling", Tiling);
+        materialPropertyBlock.SetVector("_Speed", Vector2.right * speedTexture);
+        
+        meshRenderer.SetPropertyBlock(materialPropertyBlock);
+    }
+#if UNITY_EDITOR
+    private void OnValidate()
+    {
+        InitPropertyBlock();
+    }
+#endif
 }
