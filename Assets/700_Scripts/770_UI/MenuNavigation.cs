6c9b43df4b579186275f0b2ac2488c2fb2f2fc1d
Valerian Lambert <valerian.lambert13@gmail.com>
2024-04-05 16:35:13 +0200

    Debug bug 31 (non fonctionnel)

--- Assets/700_Scripts/770_UI/MenuNavigation.cs
+++ Assets/700_Scripts/770_UI/MenuNavigation.cs
@@ -9,16 +9,6 @@ public class MenuNavigation : MonoBehaviour
 {
     public GameObject playButton;
 
-    public bool GamepadIsActive, MouseKeybordIsActive;
-    public void Start()
-    {
-        if (SwapControls.state == CurrentState.Gamepad)
-        {
-            Cursor.lockState = CursorLockMode.Locked;
-            Cursor.visible = false;
-        }
-    }
-
     private void Update()
     {
         if (SwapControls.state == CurrentState.Gamepad)
@@ -28,16 +18,12 @@ public class MenuNavigation : MonoBehaviour
                 EventSystem.current.SetSelectedGameObject(playButton);
                 Cursor.lockState = CursorLockMode.Locked;
                 Cursor.visible = false;
-                MouseKeybordIsActive = false;
-                GamepadIsActive = true;
             }
         }
         else
         {
             Cursor.lockState = CursorLockMode.None;
             Cursor.visible = true;
-            GamepadIsActive = false;
-            MouseKeybordIsActive = true;
         }
     }
 }
