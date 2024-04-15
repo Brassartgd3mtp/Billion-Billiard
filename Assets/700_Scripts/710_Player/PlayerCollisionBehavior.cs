2b84df2d115363775e91377dd8c31b4e24fc6298
Nicolas <imbert.nicolas0@gmail.com>
2024-04-15 13:47:46 +0200

    Ajout shader Fake_Light avec le prefab du projecteur

--- Assets/700_Scripts/710_Player/PlayerCollisionBehavior.cs
+++ Assets/700_Scripts/710_Player/PlayerCollisionBehavior.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor.Localization.Plugins.XLIFF.V20;
+//using UnityEditor.Localization.Plugins.XLIFF.V20;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
