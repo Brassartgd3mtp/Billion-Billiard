8039a21296a4b0dfb0c9da24e79bc86d7ed33cee
Florian BAUMANN <florian.baumann@brassart.fr>
2024-05-06 11:50:36 +0200

    Debug Bumpers
    - Modif du script en checkant la vitesse de PlayerParameters pour la collision
    - Passage d'un OnCollisionStay à un OnCollisionEnter
    - Il faudra référencer le prefab Player dans l'inspector des Bumpers
    - Changement du LD_P_Thomas pour référencer le prefab Player dans les bumpers

--- Assets/700_Scripts/720_GPE/722_Walls/Bumper.cs
+++ Assets/700_Scripts/720_GPE/722_Walls/Bumper.cs
@@ -5,11 +5,13 @@ using UnityEngine;
 public class Bumper : MonoBehaviour
 {
     [SerializeField] float baseForce = 0;
-    private void OnCollisionStay(Collision collision)
-    {
-        Vector3 direction = transform.position - PlayerController.Instance.transform.position;
+    public PlayerParameters playerParameters;
 
-        if (collision.gameObject.layer == 3 && PlayerController.rb.velocity.magnitude == 0)
+    private void OnCollisionEnter(Collision collision)
+    {
+        Vector3 direction = PlayerController.Instance.transform.position - transform.position;
+
+        if (collision.gameObject.layer == 3 && playerParameters.speed == 0)
         {
             PlayerController.rb.AddForce(direction.normalized * baseForce, ForceMode.Impulse);
         }
