4cc8f1cd282ca34a1ddfa7f85d7f430ae270a6ca
Valerian Lambert <valerian.lambert13@gmail.com>
2024-05-24 16:07:39 +0200

    Debug Traj Pred after bounce clip through walls

--- Assets/700_Scripts/770_UI/774_World/Trajectory Prediction.cs
+++ Assets/700_Scripts/770_UI/774_World/Trajectory Prediction.cs
@@ -8,75 +8,47 @@ public class TrajectoryPrediction : MonoBehaviour
     [SerializeField] private PlayerController playerController;
     [SerializeField] private LayerMask wallLayer;
     private float maxDist = 12;
-    private float sphereCollider;
+    private float sphereRadius;
 
     private void Start()
     {
         InputHandler.TrajectoryPredictionEnable(this);
 
-        sphereCollider = playerController.GetComponent<SphereCollider>().radius;
+        sphereRadius = playerController.GetComponent<SphereCollider>().radius;
     }
 
     RaycastHit hit;
+    RaycastHit bounceHit;
     private void Update()
     {
-        Ray ray = new Ray(transform.position, transform.forward);
-
         if (playerController.ThrowStrength > 0 || PlayerController.rb.velocity == Vector3.zero)
         {
-            //if (Physics.Raycast(ray, out hit, 12, wallLayer))
-            //{
-            //    lineRenderer.SetPosition(1, transform.InverseTransformPoint(hit.point));
-            //
-            //    lineRenderer.positionCount = 3;
-            //
-            //    lineRenderer.SetPosition(2, transform.InverseTransformPoint(hit.point + Vector3.Reflect(ray.direction, hit.normal) * 3));
-            //}
-            //else
-            //{
-            //    lineRenderer.positionCount = 2;
-            //
-            //    lineRenderer.SetPosition(1, Vector3.forward * 12);
-            //}
-            //
-            //if (Physics.BoxCast(transform.position + new Vector3(0, -.5f, 0), sphereCollider / 1.4f, transform.forward, out hit, Quaternion.identity, 12, wallLayer))
-            //{
-            //    Vector3 hitPointwoY = new Vector3(hit.point.x, 0, hit.point.z);
-            //
-            //    Vector3 hitNormwoY = new Vector3(hit.normal.x, 0, hit.normal.z);
-            //
-            //    lineRenderer.SetPosition(1, transform.InverseTransformPoint(hitPointwoY));
-            //
-            //    lineRenderer.positionCount = 3;
-            //
-            //    lineRenderer.SetPosition(2, transform.InverseTransformPoint(hitPointwoY + Vector3.Reflect(ray.direction, hitNormwoY) * 3));
-            //}
-            //else
-            //{
-            //    lineRenderer.positionCount = 2;
-            //
-            //    lineRenderer.SetPosition(1, Vector3.forward * 12);
-            //}
-
-            if (Physics.CapsuleCast(transform.position, transform.position, sphereCollider - .03f, transform.forward, out hit, maxDist, wallLayer))
+            if (Physics.SphereCast(transform.position, sphereRadius, transform.forward, out hit, maxDist, wallLayer))
             {
                 Vector3 hitPoint = new Vector3(hit.point.x, .3f, hit.point.z);
-                Vector3 hitPointnoY = new Vector3(hit.point.x, .3f, hit.point.z);
                 Vector3 hitNormnoY = new Vector3(hit.normal.x, 0, hit.normal.z);
 
-                float hitDist = maxDist - Vector3.Distance(hit.point, transform.position);
+                float hitDist = maxDist - hit.distance;
             
                 lineRenderer.SetPosition(1, transform.InverseTransformPoint(hitPoint));
             
                 lineRenderer.positionCount = 3;
-            
-                lineRenderer.SetPosition(2, transform.InverseTransformPoint(hitPointnoY + Vector3.Reflect(ray.direction, hitNormnoY) * (2 + hitDist)));
+
+                Vector3 bounceForward = Vector3.Reflect(transform.forward, hitNormnoY);
+
+                if (Physics.SphereCast(hitPoint, sphereRadius, bounceForward, out bounceHit, hitDist, wallLayer))
+                {
+                    Vector3 bouncePoint = new Vector3(bounceHit.point.x, .3f, bounceHit.point.z);
+                    lineRenderer.SetPosition(2, transform.InverseTransformPoint(bouncePoint));
+                }
+                else
+                    lineRenderer.SetPosition(2, transform.InverseTransformPoint(hitPoint + bounceForward * hitDist));
             }
             else
             {
                 lineRenderer.positionCount = 2;
             
-                lineRenderer.SetPosition(1, Vector3.forward * 12);
+                lineRenderer.SetPosition(1, Vector3.forward * maxDist);
             }
         }
         else
